3797f73eb93b38e4956d7a0b8609bd2c76900495
Ashley Ginsberg <aginz200@gmail.com>
2015-05-12 13:19:19 -0400

    init

--- server.rb
+++ server.rb
@@ -1,34 +1,5 @@
 require 'sinatra'
 
 post '/' do 
-  "Waka Waka"
+  "Waka Waka!"
 end
-
-
-
-
-
-# get '/' do
-#   redirect to('/stock_price.rb')
-# end
-
-# get '/sinatra' do
-#   "Hello Sinatra"
-# end
-
-# require 'sinatra'
-# require 'nokogiri'
-# require 'httparty'
-
-# response = HTTParty.get("http://finance.yahoo.com/q?s=AAPL")
-
-# parsed_response = Nokogiri::HTML(response.body)
-
-# stock_price = parsed_response.xpath("//span[@id='yfs_l84_aapl']").first.content
-# previous_stock_price = parsed_response.xpath("//td[@class='yfnc_tabledata1']").first.content
-
-
-# get '/' do
-#   "Current Stock Price: $#{stock_price}
-#   Previous Close: $#{previous_stock_price}"
-# end
